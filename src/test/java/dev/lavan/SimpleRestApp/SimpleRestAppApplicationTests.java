66c9e3e3c7bf79141d768e4433fec6d2598b84d6
LAVAN J V <lvasanthakumar@fleetstudio.com>
2024-12-10 21:24:15 +0530

    Initial setup

--- src/test/java/dev/lavan/SimpleRestApp/SimpleRestAppApplicationTests.java
+++ src/test/java/dev/lavan/SimpleRestApp/SimpleRestAppApplicationTests.java
@@ -3,11 +3,11 @@ package dev.lavan.SimpleRestApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SimpleRestAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class SimpleRestAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
